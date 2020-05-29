abaef66aa591a4aa4636d46ec8f61dad08c4248b
Matt Hope <matthew.hope396@outlook.com>
2020-05-29 18:49:21 +0100

    Updated Hostly version.

--- src/Example.Mobile.Android/MainActivity.cs
+++ src/Example.Mobile.Android/MainActivity.cs
@@ -8,15 +8,13 @@ using Android.Runtime;
 using Example.Mobile.Extensions;
 using Hostly;
 using Hostly.Extensions;
+using Hostly.Android.Extensions;
 
 namespace Example.Mobile.Droid
 {
     [Activity(Label = "Example.Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, IXamarinHostingPlatform
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        public event EventHandler OnStarted;
-        public event EventHandler OnStopped;
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -37,26 +35,6 @@ namespace Example.Mobile.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-
-        protected override void OnStop()
-        {
-            base.OnStop();
-            OnStopped(this, null);
-        }
-
-        protected override void OnStart()
-        {
-            base.OnStart();
-            OnStarted(this, null);
-        }
-
-        void IXamarinHostingPlatform.LoadApplication(IXamarinApplication application)
-        {
-            if (typeof(global::Xamarin.Forms.Application).IsAssignableFrom(application.GetType()))
-                base.LoadApplication((global::Xamarin.Forms.Application)application);
-            else
-                throw new ArgumentException("Application supplied is of incorrect type");
         }
     }
 }
